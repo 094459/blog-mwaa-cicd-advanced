0ae8086c1f0bcf0e6f1b0f53bd06ee824cea5d54
ricsue <ricsue@amazon.com>
2021-09-16 11:58:01 +0100

    new changes

---
+++ dags/airflow-timestream.py
@@ -0,0 +1,49 @@
+
+#Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#SPDX-License-Identifier: Apache-2.0
+
+import os
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+import awswrangler as wr
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+def ts_query():
+    wr.s3.to_csv(
+    df=wr.timestream.query("""
+SELECT region, availability_zone, instance_name, BIN(time, 24h) AS binned_timestamp,
+    ROUND(AVG(measure_value::double), 2) AS avg_cpu_utilization,
+    ROUND(APPROX_PERCENTILE(measure_value::double, 0.9), 2) AS p90_cpu_utilization,
+    ROUND(APPROX_PERCENTILE(measure_value::double, 0.95), 2) AS p95_cpu_utilization,
+    ROUND(APPROX_PERCENTILE(measure_value::double, 0.99), 2) AS p99_cpu_utilization
+FROM "demoAirflow".kinesisdata1
+WHERE measure_name = 'cpu_system'
+    AND time > ago(24h)
+GROUP BY region, instance_name, availability_zone, BIN(time, 24h)
+ORDER BY binned_timestamp ASC
+"""),
+    path='s3://demo-airflow-ts-output/wr/my_file.csv',
+)
+
+with DAG(
+        dag_id=os.path.basename(__file__).replace(".py", ""),
+        default_args=default_args,
+        dagrun_timeout=timedelta(hours=2),
+        schedule_interval=None
+) as dag:
+
+    ts_query=PythonOperator(task_id='ts_query', python_callable=ts_query, dag=dag)
+    ts_query
